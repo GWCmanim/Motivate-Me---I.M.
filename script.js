337d49b7d3fefb51a0d1181f2de2f3f0a7ab4d28
ImaniMitchell <>
2021-07-19 20:11:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Add your JavaScript here*/
+var clickCount= 3;
+
+var motivationGenerator = document.getElementById ("motivationGenerator");
+
+
+motivationGenerator.addEventListener("click", function_here);
+
+
+function function_here(){
+ alert("hi, how are ya?");
+}
+
+
+
+
+
+
